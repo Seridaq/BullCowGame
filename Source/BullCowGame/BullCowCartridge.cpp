4abc3fd133bab59f8852b6cfcf57cc409fb50b60
Thomas <tebruhnke@gmail.com>
2021-06-24 23:28:28 -0500

    Updated game loop
    Implemented loosing lives, game over (lose or win), restarting the game, and length checking.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,21 +4,84 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    HiddenWord = TEXT("cake");
-    PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Guess the 4 letter word.")); // TODO: Magic Number Remove!
-    PrintLine(TEXT("Press enter to continue..."));
+    SetupGame();
+
+    PrintLine(TEXT("The HiddenWord is: %s"), *HiddenWord); // Debug Line
+    PrintLine(TEXT("It is %i characters long."), HiddenWord.Len()); // Debug line
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-    if(Input == HiddenWord)
+    if(bGameOver)
     {
-        PrintLine(TEXT("You have won!"));
+        ClearScreen();
+        SetupGame();
+        return;
+    }
+    ProcessGuess(Input);
+    
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+
+    Level = 1;
+
+    // TODO: Get random isogram from list based off of level
+    // Initialize isogram TODO: Make equal to random isogram from list.
+    HiddenWord = TEXT("cake");
+    
+    Lives = Level + 3;
+    bGameOver = false;
+    
+    PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
+    PrintLine(TEXT("Type in your guess.\nPress enter to continue..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press Enter to play again."));
+}
+
+void UBullCowCartridge::ProcessGuess(const FString Guess)
+{
+    if(Guess == HiddenWord)
+    {
+        PrintLine(TEXT("Congratulations!"));
+        PrintLine(TEXT("You have guessed the correct word."));
+        EndGame();
+
+        if(++Level > 5)
+        {
+            PrintLine("You have won the game!");
+            EndGame();
+        }
+
+        // If No Initialize lives and word based on new level
     }
     else
-    {
-        PrintLine(TEXT("You have lost!"));
-    }
+        {
+        if(HiddenWord.Len() != Guess.Len())
+        {
+            PrintLine(TEXT("The hidden word is %i characters long, try again."), HiddenWord.Len());
+            return;
+        }
+
+        // Check if isogram
+
+        PrintLine(TEXT("You have lost a life!"));
+        PrintLine(TEXT("You have %i lives remaining."), --Lives);
+
+        if(Lives <= 0)
+        {
+            PrintLine(TEXT("You lose!"));
+            PrintLine(TEXT("The current hidden word was \"%s\"."), *HiddenWord);
+            EndGame();
+            return;
+        }
+        // Check bulls and cows, display to the player.
+        PrintLine("Please try again.");
+        }
 }
